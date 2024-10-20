a9567d6ba85ff77c30222c199fc9cbb3198a49ff
Josh Livingston <joshwlivingston@gmail.com>
2024-10-20 11:28:57 -0600

    django polls app from django tutorial
    main app: https://github.com/joshwlivingston/django-tutorial

---
+++ django_polls/models.py
@@ -0,0 +1,35 @@
+import datetime
+
+from django.contrib import admin
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Question(models.Model):
+    # object name = field name. column name
+    question_text = models.CharField(max_length=200)
+    
+    # if supplied, first unnamed argument is human friendly name
+    pub_date = models.DateTimeField("date published")
+    
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?"
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def __str__(self):
+        return self.question_text
+    
+
+class Choice(models.Model):
+    # can also define relationships
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
